74dc6bd14a045d75ab2bf470dfe243cf9426f312
Irin <averyirin@gmail.com>
2017-06-30 18:51:12 -0400

    adjusted client api routing

---
+++ LocalAccountsApp/Controllers/ClientController.cs
@@ -0,0 +1,28 @@
+﻿using LocalAccountsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LocalAccountsApp.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Clients")]
+    public class ClientController : ApiController
+    {
+
+        // GET api/Clients
+        public object Get()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var clients = context.Database.SqlQuery<object>("SELECT * from dbo.Clients").ToList();
+                return clients;
+            }
+        }
+        
+    }
+}
